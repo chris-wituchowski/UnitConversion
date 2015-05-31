7d4ff8cf0f8ee6ba3ea3224b2faa11b63584106d
chris-wituchowski <c.wituchowski@gmail.com>
2015-05-30 21:31:49 -0700

    Added output radio group with buttons Added new radio buttons for pressure units Added hints to input and output edittexts Modified layout to accommodate new radio group Added rest of cases in inputRadioGroup switch statement Added new switch statement for outputRadioGroup Added required variables for outputRadioGroup Changed conversion logic to first convert input to Pa, then to selected output unit before displaying in output edittext.

--- app/src/main/java/com/example/chris/unitconversion/MainActivityFragment.java
+++ app/src/main/java/com/example/chris/unitconversion/MainActivityFragment.java
@@ -1,6 +1,5 @@
 package com.example.chris.unitconversion;
 
-import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.text.Editable;
@@ -21,6 +20,7 @@ public class MainActivityFragment extends Fragment implements TextWatcher {
     private EditText mInputEditText;
     private EditText mOutputEditText;
     private RadioGroup mInputRadioGroup;
+    private RadioGroup mOutputRadioGroup;
 
     public MainActivityFragment() {
     }
@@ -37,17 +37,17 @@ public class MainActivityFragment extends Fragment implements TextWatcher {
         super.onViewCreated(view, savedInstanceState);
 
         EditText inputEditText = (EditText) view.findViewById(R.id.inputNumber);
-        inputEditText.setText("stuff");
         inputEditText.addTextChangedListener(this);
 
         EditText outputEditText = (EditText) view.findViewById(R.id.outputNumber);
-        outputEditText.setText("morestuff");
 
         RadioGroup inputRadioGroup = (RadioGroup) view.findViewById(R.id.inputRadioGroup);
+        RadioGroup outputRadioGroup = (RadioGroup) view.findViewById(R.id.outputRadioGroup);
 
         mInputEditText = inputEditText;
         mOutputEditText = outputEditText;
         mInputRadioGroup = inputRadioGroup;
+        mOutputRadioGroup = outputRadioGroup;
     }
 
     // TextWatcher
@@ -73,22 +73,78 @@ public class MainActivityFragment extends Fragment implements TextWatcher {
         {
             Double inputNumber = Double.parseDouble(inputString);
             double conversionFactor = 0.0;
+            double initialConvertedNumber;
 
             RadioGroup inputRadioGroup = mInputRadioGroup;
             int inputRadioButtonId = inputRadioGroup.getCheckedRadioButtonId();
             switch(inputRadioButtonId)
             {
-                case R.id.psi:
+                case R.id.inPsi:
                     Log.d("Chris", "psi");
-                    conversionFactor = 6.0;
+                    conversionFactor = 6894.75729;
                     break;
-                case R.id.bar:
+                case R.id.inBar:
                     Log.d("Chris", "bar");
-                    conversionFactor = 3.0;
+                    conversionFactor = 100000.0;
+                    break;
+                case R.id.inAt:
+                    Log.d("Chris", "at");
+                    conversionFactor = 98066.5;
+                    break;
+                case R.id.inAtm:
+                    Log.d("Chris", "atm");
+                    conversionFactor = 101325.0;
+                    break;
+                case R.id.inKPa:
+                    Log.d("Chris", "kpa");
+                    conversionFactor = 1000;
+                    break;
+                case R.id.inMPa:
+                    Log.d("Chris", "mpa");
+                    conversionFactor = 1000000;
+                    break;
+                case R.id.inTorr:
+                    Log.d("Chris", "torr");
+                    conversionFactor = 133.3224;
                     break;
             }
 
-            Double outputNumber = inputNumber * conversionFactor;
+            initialConvertedNumber = inputNumber * conversionFactor;
+
+            RadioGroup outputRadioGroup = mOutputRadioGroup;
+            int outputRadioButtonId = outputRadioGroup.getCheckedRadioButtonId();
+            switch (outputRadioButtonId) {
+                case R.id.outPsi:
+                    Log.d("Chris", "psi");
+                    conversionFactor = 0.00014503;
+                    break;
+                case R.id.outBar:
+                    Log.d("Chris", "bar");
+                    conversionFactor = 0.00001;
+                    break;
+                case R.id.outAt:
+                    Log.d("Chris", "at");
+                    conversionFactor = 0.000010197;
+                    break;
+                case R.id.outAtm:
+                    Log.d("Chris", "atm");
+                    conversionFactor = 0.0000098692;
+                    break;
+                case R.id.outKPa:
+                    Log.d("Chris", "kpa");
+                    conversionFactor = 0.001;
+                    break;
+                case R.id.outMPa:
+                    Log.d("Chris", "mpa");
+                    conversionFactor = 0.000001;
+                    break;
+                case R.id.outTorr:
+                    Log.d("Chris", "torr");
+                    conversionFactor = 0.0075006;
+                    break;
+            }
+
+            Double outputNumber = initialConvertedNumber * conversionFactor;
 
             outputString = outputNumber.toString();
         }
