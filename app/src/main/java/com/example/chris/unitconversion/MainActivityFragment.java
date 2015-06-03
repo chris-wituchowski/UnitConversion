ca86df450460efee4a9432f1f0ec33c9c4b0317a
chris-wituchowski <c.wituchowski@gmail.com>
2015-06-02 21:26:59 -0700

    Created new method called convertUnits and moved all conversion code from afterTextChanged to convertUnits. Created listener for input and output radio groups so that conversion occurs when selecting a new unit on either the input or output side. Started work on comparing the input and output radio group ID to ignore conversion when the same input and output units are selected.

--- app/src/main/java/com/example/chris/unitconversion/MainActivityFragment.java
+++ app/src/main/java/com/example/chris/unitconversion/MainActivityFragment.java
@@ -1,5 +1,6 @@
 package com.example.chris.unitconversion;
 
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.text.Editable;
@@ -9,13 +10,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment implements TextWatcher {
+public class MainActivityFragment extends Fragment implements TextWatcher, RadioGroup.OnCheckedChangeListener {
 
     private EditText mInputEditText;
     private EditText mOutputEditText;
@@ -44,10 +46,19 @@ public class MainActivityFragment extends Fragment implements TextWatcher {
         RadioGroup inputRadioGroup = (RadioGroup) view.findViewById(R.id.inputRadioGroup);
         RadioGroup outputRadioGroup = (RadioGroup) view.findViewById(R.id.outputRadioGroup);
 
+        inputRadioGroup.setOnCheckedChangeListener(this);
+        outputRadioGroup.setOnCheckedChangeListener(this);
+
         mInputEditText = inputEditText;
         mOutputEditText = outputEditText;
         mInputRadioGroup = inputRadioGroup;
         mOutputRadioGroup = outputRadioGroup;
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        Log.d("RadioGroup", "changed");
+        convertUnits();
     }
 
     // TextWatcher
@@ -62,95 +73,110 @@ public class MainActivityFragment extends Fragment implements TextWatcher {
     }
 
     @Override
-    public void afterTextChanged(Editable s)
-    {
+    public void afterTextChanged(Editable s) {
+        convertUnits();
+    }
+
+    public void convertUnits() {
         Editable inputEditText = mInputEditText.getText();
         String inputString = inputEditText.toString();
 
         //convert to double, need try/catch in case input EditText is not a number
         String outputString;
-        try
-        {
+        try {
             Double inputNumber = Double.parseDouble(inputString);
             double conversionFactor = 0.0;
             double initialConvertedNumber;
 
             RadioGroup inputRadioGroup = mInputRadioGroup;
             int inputRadioButtonId = inputRadioGroup.getCheckedRadioButtonId();
-            switch(inputRadioButtonId)
-            {
-                case R.id.inPsi:
-                    Log.d("Chris", "psi");
-                    conversionFactor = 6894.75729;
-                    break;
-                case R.id.inBar:
-                    Log.d("Chris", "bar");
-                    conversionFactor = 100000.0;
-                    break;
-                case R.id.inAt:
-                    Log.d("Chris", "at");
-                    conversionFactor = 98066.5;
-                    break;
-                case R.id.inAtm:
-                    Log.d("Chris", "atm");
-                    conversionFactor = 101325.0;
-                    break;
-                case R.id.inKPa:
-                    Log.d("Chris", "kpa");
-                    conversionFactor = 1000;
-                    break;
-                case R.id.inMPa:
-                    Log.d("Chris", "mpa");
-                    conversionFactor = 1000000;
-                    break;
-                case R.id.inTorr:
-                    Log.d("Chris", "torr");
-                    conversionFactor = 133.3224;
-                    break;
-            }
-
-            initialConvertedNumber = inputNumber * conversionFactor;
-
             RadioGroup outputRadioGroup = mOutputRadioGroup;
             int outputRadioButtonId = outputRadioGroup.getCheckedRadioButtonId();
-            switch (outputRadioButtonId) {
-                case R.id.outPsi:
-                    Log.d("Chris", "psi");
-                    conversionFactor = 0.00014503;
-                    break;
-                case R.id.outBar:
-                    Log.d("Chris", "bar");
-                    conversionFactor = 0.00001;
-                    break;
-                case R.id.outAt:
-                    Log.d("Chris", "at");
-                    conversionFactor = 0.000010197;
-                    break;
-                case R.id.outAtm:
-                    Log.d("Chris", "atm");
-                    conversionFactor = 0.0000098692;
-                    break;
-                case R.id.outKPa:
-                    Log.d("Chris", "kpa");
-                    conversionFactor = 0.001;
-                    break;
-                case R.id.outMPa:
-                    Log.d("Chris", "mpa");
-                    conversionFactor = 0.000001;
-                    break;
-                case R.id.outTorr:
-                    Log.d("Chris", "torr");
-                    conversionFactor = 0.0075006;
-                    break;
+
+            Integer inputRadioButtonIdInteger = inputRadioButtonId;
+            Integer outputRadioButtonIdInteger = outputRadioButtonId;
+
+            Log.d("input button id", inputRadioButtonIdInteger.toString());
+            Log.d("output button id", outputRadioButtonIdInteger.toString());
+
+            if (inputRadioButtonId == outputRadioButtonId)
+            {
+                outputString = inputString;
+            }
+            else
+            {
+                switch (inputRadioButtonId) {
+                    case R.id.inPsi:
+                        Log.d("Input", "psi");
+                        conversionFactor = 6894.75729;
+                        break;
+                    case R.id.inBar:
+                        Log.d("Input", "bar");
+                        conversionFactor = 100000.0;
+                        break;
+                    case R.id.inAt:
+                        Log.d("Input", "at");
+                        conversionFactor = 98066.5;
+                        break;
+                    case R.id.inAtm:
+                        Log.d("Input", "atm");
+                        conversionFactor = 101325.0;
+                        break;
+                    case R.id.inKPa:
+                        Log.d("Input", "kpa");
+                        conversionFactor = 1000;
+                        break;
+                    case R.id.inMPa:
+                        Log.d("Input", "mpa");
+                        conversionFactor = 1000000;
+                        break;
+                    case R.id.inTorr:
+                        Log.d("Input", "torr");
+                        conversionFactor = 133.3224;
+                        break;
+                }
+
+                initialConvertedNumber = inputNumber * conversionFactor;
+
+                switch (outputRadioButtonId) {
+                    case R.id.outPsi:
+                        Log.d("Output", "psi");
+                        conversionFactor = 0.00014503;
+                        break;
+                    case R.id.outBar:
+                        Log.d("Output", "bar");
+                        conversionFactor = 0.00001;
+                        break;
+                    case R.id.outAt:
+                        Log.d("Output", "at");
+                        conversionFactor = 0.000010197;
+                        break;
+                    case R.id.outAtm:
+                        Log.d("Output", "atm");
+                        conversionFactor = 0.0000098692;
+                        break;
+                    case R.id.outKPa:
+                        Log.d("Output", "kpa");
+                        conversionFactor = 0.001;
+                        break;
+                    case R.id.outMPa:
+                        Log.d("Output", "mpa");
+                        conversionFactor = 0.000001;
+                        break;
+                    case R.id.outTorr:
+                        Log.d("Output", "torr");
+                        conversionFactor = 0.0075006;
+                        break;
+                }
+
+                Double outputNumber = initialConvertedNumber * conversionFactor;
+
+                outputString = outputNumber.toString();
             }
 
-            Double outputNumber = initialConvertedNumber * conversionFactor;
-
-            outputString = outputNumber.toString();
         }
-        catch (NumberFormatException e)
-        {
-            outputString = "NaN";
+        catch (NumberFormatException e) {
+            outputString = "";
         }
 
 
