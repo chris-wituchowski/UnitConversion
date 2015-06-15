98db47ec92c50677eb47c90db98b047a9a6ee157
chris-wituchowski <c.wituchowski@gmail.com>
2015-06-14 23:41:18 -0700

    Architectural change
    Changed code so that radio buttons are dynamically created based on the
    number of units in a unitList array. This is to get the code ready for
    when additional unit types are added.

---
+++ app/src/main/java/com/example/chris/unitconversion/data/UnitData.java
@@ -0,0 +1,30 @@
+package com.example.chris.unitconversion.data;
+
+/**
+ * Created by chris on 6/14/2015.
+ */
+public class UnitData {
+    private String mName;
+    private double mToConversionFactor;
+    private double mFromConversionFactor;
+
+    public String getName() {
+        return mName;
+    }
+
+    public UnitData(String data, double toConversionFactor, double fromConversionFactor) {
+        mName = data;
+        mToConversionFactor = toConversionFactor;
+        mFromConversionFactor = fromConversionFactor;
+
+    }
+
+    public double toConversionFactor(double value) {
+        return value*mToConversionFactor;
+    }
+
+    public double fromConversionFactor(double value) {
+        return value*mFromConversionFactor;
+    }
+
+}
